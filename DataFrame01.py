ea22ac4c48ab9aa0a134ae92bc856b3e0e5a806a
Arnaud TOUSSAINT <ArnaudToussaint@users.noreply.github.com>
2024-07-03 15:53:40 +0200

    Update DataFrame01.py

--- DataFrame01.py
+++ DataFrame01.py
@@ -11,5 +11,5 @@ df = pd.DataFrame(np.random.randn(10, 20), columns=("col %d" % i for i in range(
 st.dataframe(df.style.highlight_max(axis=0))
 
 data = requests.get("'https://jsonplaceholder.typicode.com/todos/1'").json()
-
+#https://www.hatvp.fr/agora/opendata/agora_repertoire_opendata.json
 st.write(data)
