fdd3dbffddfe6c1d3e58396566725e82c43da1bf
Arnaud TOUSSAINT <ArnaudToussaint@users.noreply.github.com>
2024-07-03 16:45:39 +0200

    Update DataFrame01.py

--- DataFrame01.py
+++ DataFrame01.py
@@ -10,6 +10,7 @@ import json
 
 url1 = 'https://raw.githubusercontent.com/insightbuilder/python_de_learners_data/main/code_script_notebooks/python_scripts/json_reader/toplevel_comment_zGAkhN1YZXM.json'
 url2 = 'https://www.hatvp.fr/agora/opendata/agora_repertoire_opendata.json'
+url3 = 'https://api.gouv.fr/api/v1/apis'
 
-response = requests.get(url2).json()
+response = requests.get(url3).json()
 st.dataframe(response)
