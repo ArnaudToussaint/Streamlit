92f18fa52d59e291654cbfdd1d6ca98340fea63c
Arnaud TOUSSAINT <ArnaudToussaint@users.noreply.github.com>
2024-07-03 15:48:12 +0200

    Update DataFrame01.py

--- DataFrame01.py
+++ DataFrame01.py
@@ -1,9 +1,15 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+
+import requests
 import json
 
 
 df = pd.DataFrame(np.random.randn(10, 20), columns=("col %d" % i for i in range(20)))
 
 st.dataframe(df.style.highlight_max(axis=0))
+
+data = requests.get("'https://jsonplaceholder.typicode.com/todos/1'").json()
+
+st.write(data)
