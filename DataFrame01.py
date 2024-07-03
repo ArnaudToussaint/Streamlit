76b25449e8d653d1cad8ea11d33ae8088fa874e9
Arnaud TOUSSAINT <ArnaudToussaint@users.noreply.github.com>
2024-07-03 16:13:03 +0200

    Update DataFrame01.py

--- DataFrame01.py
+++ DataFrame01.py
@@ -5,16 +5,11 @@ import numpy as np
 import requests
 import json
 
-
-df = pd.DataFrame(np.random.randn(10, 20), columns=("col %d" % i for i in range(20)))
-
-st.dataframe(df.style.highlight_max(axis=0))
-
-#data = requests.get("'https://jsonplaceholder.typicode.com/todos/1'").json()
-#st.write(data)
+#df = pd.DataFrame(np.random.randn(10, 20), columns=("col %d" % i for i in range(20)))
+#st.dataframe(df.style.highlight_max(axis=0))
 
 url1 = 'https://raw.githubusercontent.com/insightbuilder/python_de_learners_data/main/code_script_notebooks/python_scripts/json_reader/toplevel_comment_zGAkhN1YZXM.json'
 url2 = 'https://www.hatvp.fr/agora/opendata/agora_repertoire_opendata.json'
 
-response = requests.get(url1).json()
+response = requests.get(url2).json()
 st.dataframe(response)
