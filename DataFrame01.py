bb1f9e70d71b0424b4090f9e731bc6a6a4b64f7b
Arnaud TOUSSAINT <ArnaudToussaint@users.noreply.github.com>
2024-07-03 16:11:11 +0200

    Update DataFrame01.py

--- DataFrame01.py
+++ DataFrame01.py
@@ -10,10 +10,12 @@ df = pd.DataFrame(np.random.randn(10, 20), columns=("col %d" % i for i in range(
 
 st.dataframe(df.style.highlight_max(axis=0))
 
-data = requests.get("'https://jsonplaceholder.typicode.com/todos/1'").json()
+#data = requests.get("'https://jsonplaceholder.typicode.com/todos/1'").json()
 #st.write(data)
 
 url1 = 'https://raw.githubusercontent.com/insightbuilder/python_de_learners_data/main/code_script_notebooks/python_scripts/json_reader/toplevel_comment_zGAkhN1YZXM.json'
 url2 = 'https://www.hatvp.fr/agora/opendata/agora_repertoire_opendata.json'
+
+response = requests.get(url1)
 
 #st.dataframe()
