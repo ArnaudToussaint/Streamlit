5fac46b9ec4065b270b24528d36cb32c3354b435
Arnaud TOUSSAINT <ArnaudToussaint@users.noreply.github.com>
2024-07-04 17:28:40 +0200

    Update map01.py

--- map01.py
+++ map01.py
@@ -2,15 +2,37 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-df = pd.DataFrame({
-    "col1": np.random.randn(1000) / 50 + 37.76,
-    "col2": np.random.randn(1000) / 50 + -122.4,
-    "col3": np.random.randn(1000) * 100,
-    "col4": np.random.rand(1000, 4).tolist(),
-})
+import json
+import requests
 
-st.map(df,
-    latitude='col1',
-    longitude='col2',
-    size='col3',
-    color='col4')
+st.set_page_config(page_title="Test", layout='wide')
+
+data_url='https://geo.api.gouv.fr/communes?codePostal=78260&fields=nom,code,codesPostaux,centre,surface,contour,bbox,codeDepartement,departement,codeRegion,region,population,zone&format=json&geometry=centre'
+response = requests.get(data_url).json()
+
+df = pd.DataFrame(response)
+
+st.dataframe(
+  data=df, 
+  #width=None, 
+  #height=None, 
+  #*, 
+  use_container_width=True, 
+  hide_index=True, 
+  column_order=None, 
+  column_config=None,  
+  key=None, 
+  on_select="ignore", 
+  selection_mode="multi-row")
+
+#df = pd.DataFrame({
+#    "col1": np.random.randn(1000) / 50 + 37.76,
+#    "col2": np.random.randn(1000) / 50 + -122.4,
+#    "col3": np.random.randn(1000) * 100,
+#    "col4": np.random.rand(1000, 4).tolist(),
+#})
+
+#st.map(df,
+#    latitude='col1',
+#    longitude='col2'
+#    )
