d9fcbc4d9745e360f133027245643470edfc4324
Arnaud TOUSSAINT <ArnaudToussaint@users.noreply.github.com>
2024-07-05 10:44:18 +0200

    Update map01.py

--- map01.py
+++ map01.py
@@ -27,7 +27,7 @@ df = pd.DataFrame(response)
 try: 
   data = pd.DataFrame(response)
   st.write(data['nom'])
-  x = data[0]["centre"]["coordinates"][0]
+  x = response[0]["centre"]["coordinates"][0]
   st.write(x)
 except:
   st.write("An exception occurred")
